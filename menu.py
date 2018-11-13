1b62ea49d1372628ad11d632312ffa96d83c98e2
Eduardo Marquez <nose238@gmail.com>
2018-11-13 12:11:44 -0600

    code is commented in a better way

--- menu.py
+++ menu.py
@@ -4,7 +4,7 @@
 ################################################################
 # This Code Has Been Developed By                              #
 #Jozic Espinoza  -- AKA HackeMatte                             #
-#Eduardo Marquez                                               #
+#Eduardo Marquez -- nose238@hotmail.com                        #
 ################################################################
 # Start code #
 ##############
@@ -26,15 +26,15 @@
 # 4. "f" variable can be used to debug bash code              #
 #*************************************************************#
 
-#####Operating System libraries#####
 import commands
 import socket
 
 
-#####End Operating System libraries# section####
 print("You can press ctrl + c in any moment to cancel this prossess.\nPress enter to continue.")
-#this line could be usefull, . To create public key
+# Public key is generated
 f = commands.getoutput("yes y | ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N '' ; chmod 600 /root/.ssh/id_rsa ") 
+
+### Get all client informaton and write .txt's which contain it 
 pcName = socket.gethostname()
 userName = raw_input("Enter your user name (it must be able to access through ssh protocol): ")
 passwd = raw_input("Enter your passwd: ")
@@ -42,18 +42,10 @@ groupName = raw_input("Enter the group name to create database: ")
 ipAddress = raw_input("Enter your ip address: ")
 port = raw_input("Enter your port (SSH): ")
 description = raw_input("Enter a description: ")
-#interfaceName = raw_input("Enter the network interface name to start diagnostic: ")
-########create files#######
-#file = open('/root/freeBSD_Files/interfaceName.txt', 'w')
-#file.write(interfaceName) #Change the hostname on data document
-#file.close()
 file = open('/root/freeBSD_Files/groupName.txt', 'w')
 file.write(groupName) #Change the hostname on data document
 file.close()
-#ipCommand="ifconfig "+str(interfaceName)+" | grep \"inet:\" | cut -d ':' -f 2 | cut -d ' ' -f 1"
-#getIp=commands.getoutput(ipCommand)
 file = open('/root/freeBSD_Files/ipHost.txt', 'w')
-#file.write(getIp) #Change the hostname on data document
 file.write(ipAddress)
 file.close()
 file = open('/root/freeBSD_Files/userName.txt', 'w')
@@ -71,6 +63,7 @@ file.close()
 file = open('/root/freeBSD_Files/port.txt', 'w')
 file.write(port)
 file.close()
+
 ###### Format and write client information
 f = open('/root/freeBSD_Files/groupName.txt', 'r') #Variable for client name
 line=f.readline()
@@ -91,7 +84,7 @@ file.close()
 file = open('/root/freeBSD_Files/clientCredentials.txt', 'w')
 file.write(ip+"|"+userName+"|"+passwd+"|"+port+"\n")
 file.close()
-######Reading server data
+###### Reading server data
 file = open("/root/freeBSD_Files/portServer.txt", "r")
 portServer = file.read()
 file.close()
@@ -101,14 +94,17 @@ file.close()
 file = open("/root/freeBSD_Files/ipServer.txt", "r")
 ipServer = file.read()
 file.close()
-######Send Public key to the server. IT NEEDS TO TYPE 2 TIMES PASSWORD
+###### Send Public key to the server. IT NEEDS TO TYPE 2 TIMES PASSWORD
 print("Enter the server's password to copy public key")
+
+# It generates a temp file with id_rsa.pub. Then it is added to the server's authorized_keys file
 f = commands.getoutput("scp -o StrictHostKeyChecking=no -P "+portServer+" /root/.ssh/id_rsa.pub "+userServer+"@"+ipServer+":.ssh/temp/")
 print(f)
 print("Public key has been copied. Enter the server's password again to apply last modifications")
 f = commands.getoutput("ssh "+userServer+"@"+ipServer+" -p "+portServer+" 'cat .ssh/temp/id_rsa.pub >> .ssh/authorized_keys '")
 print(f)
-######Once public key is on the server, the group directory is created and  copy information.
+
+###### Once public key is on the server, the group directory is created and  copy information.
 	# verify if group and ip directories exist.
 f = commands.getoutput("ssh "+userServer+"@"+ipServer+" -p "+portServer+"                               \
 	'if [ -d /var/www/html/centralizedConsole/web/clients/"+groupName+"  ];                             \
@@ -130,6 +126,7 @@ f = commands.getoutput("scp -P "+portServer+" /root/freeBSD_Files/clientInformat
 	"/"+ip+"/" )
 print(f)
 
+# Client's credential is added in clientsCredentials File
 f = commands.getoutput("ssh "+userServer+"@"+ipServer+" -p "+portServer+" 'cat /var/www/html/centralizedConsole/web/clients/"+
 	groupName+"/"+ip+"/clientCredentials.txt >> /var/www/html/centralizedConsole/web/clients/clientsCredentials '" )
 print(f)
