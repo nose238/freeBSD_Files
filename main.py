ad5670f6edc7a5f6d0968aca5b503c53f107b3e0
Eduardo <nose238@hotmail.com>
2019-02-18 17:52:03 -0600

    almost

--- main.py
+++ main.py
@@ -7,6 +7,9 @@
 ################################################################
 import commands
 
+
+
+
 # Read server data.
 tmp_file = open("portServer.txt", "r")
 server_port = tmp_file.read()
@@ -17,6 +20,20 @@ tmp_file.close()
 tmp_file = open("ipServer.txt", "r")
 server_ip = tmp_file.read()
 tmp_file.close()
+
+
+
+# function to share the public key to the server
+def ssh_key(user):
+	key_status = commands.getoutput("yes y | ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N ''  ")
+	print("ssh-keygen...... Generated")
+	key_status = commands.getoutput("scp -o StrictHostKeyChecking=no -P "+server_port+
+		" /root/.ssh/id_rsa.pub "+user+"@"+server_ip+":.ssh/temp/")
+	print("Public key has been copied. Enter the server's password again to apply last modifications")
+	key_status = commands.getoutput("ssh "+user+"@"+server_ip+" -p "+server_port+" 'cat .ssh/temp/id_rsa.pub >> .ssh/authorized_keys '")
+	print(key_status)
+
+
 
 
 
@@ -35,7 +52,7 @@ while incorrect_option:
 		while group_exists: 
 			group_name = str(raw_input("Enter the group's name: "))
 			group_status = commands.getoutput("ssh "+server_user+"@"+server_ip+" -p "+server_port+"\
-				'if [ -d /var/www/html/centralizedConsole/web/clients/"+group_name+"  ];     \
+				'if [ -d /home/"+group_name+"  ];     \
 				then echo \"existent\"; else echo \"nonexistent\" ; fi ; ' ")
 			print("This group is " + group_status)
 			if group_status == "existent":
@@ -57,10 +74,22 @@ while incorrect_option:
 					if user_group_pass == confirm_pass:
 						pass_doesnot_match = False
 						print("Passwords match!!! We can continue with the configuration")
-						user_group_status = commands.getoutput("ssh "+server_user+"@"+server_ip+" -p "+server_port+"\
-							'useradd "+group_name+" -d /var/www/html/centralizedConsole/web/clients/"+group_name+" ; \
-							echo \""+user_group_pass+"\" | passwd "+group_name+" --stdin ; ' ")
-						print(user_group_status)
+						user_group_status = commands.getoutput("ssh "+server_user+"@"+server_ip+" -p "+server_port+" \
+							'useradd "+group_name+" ; \
+							echo \""+user_group_pass+"\" | passwd "+group_name+" --stdin ; \
+							mkdir /var/www/html/centralizedConsole/web/clients/"+group_name+" ; \
+							chmod 700 /var/www/html/centralizedConsole/web/clients/"+group_name+" ; \
+							chown "+group_name+" /var/www/html/centralizedConsole/web/clients/"+group_name+"/ ; \
+							ln -s /var/www/html/centralizedConsole/web/clients/"+group_name+"/ /home/"+group_name+"/xml ; \
+							mkdir /home/"+group_name+"/.ssh ; chmod 700 /home/"+group_name+"/.ssh/ ; \
+							chown "+group_name+" /home/"+group_name+"/.ssh ;  \
+							touch /home/"+group_name+"/.ssh/authorized_keys ; \
+							chmod 600 /home/"+group_name+"/.ssh/authorized_keys ; \
+							chown "+group_name+" /home/"+group_name+"/.ssh/authorized_keys ; \
+							mkdir  /home/"+group_name+"/.ssh/temp ;  \
+							chown "+group_name+" /home/"+group_name+"/.ssh/temp ; ' ")
+						ssh_key(group_name)
+						# You can print out the user_group_status variable if a promlem is happening to know why.
 					else:
 						print("Passwords don't match.")
 						# Loop is repeted
@@ -71,7 +100,8 @@ while incorrect_option:
 	elif option == "2": 
 		incorrect_option = False
 		print("\nYou chose Join to a existent group.")
-		continue
+		group_name = str(raw_input("Enter the group's name you want to join to: "))
+
 
 	else: # ERROR
 		option = str(input("\nChoose a correct option.\n1. Create a new group.\n2. Join to a existent group.\n"))
