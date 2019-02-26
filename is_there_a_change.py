81ffdac4e9b12b54c0b5a14a12f505c8dab86733
Eduardo <nose238@hotmail.com>
2019-02-25 18:14:35 -0600

    python Daemon

--- is_there_a_change.py
+++ is_there_a_change.py
@@ -50,14 +50,73 @@ class App():
 					ips_to_change = commands.getoutput("ssh "+group_name+"@"+server_ip+" -p "+server_port+"\
 						'if [ -f /home/"+group_name+"/ips_to_change.txt  ];     \
 						then echo \"existent\"; else echo \"nonexistent\" ; fi ; ' ")
+					
 					if ips_to_change == "existent":
 						download_txt = commands.getstatusoutput("scp -o StrictHostKeyChecking=no -P "+server_port+
 							" "+group_name+"@"+server_ip+":ips_to_change.txt /root/freeBSD_Files/")
+						# delete_txt = commands.getstatusoutput("ssh "+group_name+"@"+server_ip+" -p "+server_port+" \
+						# 	'rm -f ips_to_change.txt ; ' ")
+
 						if download_txt[0] != 0:
 							print("It was not possible to download the file")
-						else:
+						else: # TXT was downloaded
 							ips_to_change = False
 							
+							# This code is going to search the client's IP in order to know if a change is required
+							with open("/root/freeBSD_Files/my_ip.txt", "r") as my_ip:
+								ip_to_find = my_ip.read()
+							with open("/root/freeBSD_Files/ips_to_change.txt", "r") as ips_to_change:
+								all_txt = ips_to_change.read()
+								ips_to_change.seek(0)
+								end_aux = 0
+								the_begin = ""
+								start_aux = 0
+								for line in ips_to_change:
+									end_aux += len(line)
+									if line[:-1] == ip_to_find:
+										start_aux = end_aux - len(line)
+										ip_found = True
+										break
+									the_begin += line
+								ips_to_change.seek(end_aux)
+								the_rest = ips_to_change.read()
+								ips_to_change.seek(start_aux)
+
+							with open("/root/freeBSD_Files/ips_to_change.txt", "w") as ips_to_change:
+								ips_to_change.write(the_begin)
+								ips_to_change.write(the_rest)
+
+							empty_txt = False
+							with open("/root/freeBSD_Files/ips_to_change.txt", "r") as ips_to_change:
+								all_txt = ips_to_change.read()
+								if all_txt == "":
+									empty_txt = True
+
+							if ip_found:
+								if empty_txt:
+									# Elim
+									delete_txt = commands.getstatusoutput("ssh "+group_name+"@"+server_ip+" -p "+server_port+" \
+									 	'rm -f ips_to_change.txt ; ' ")
+									break
+								else:
+									# Send to the server the new txt
+									upload_txt = commands.getstatusoutput("scp -o StrictHostKeyChecking=no -P "+server_port+
+										" /root/freeBSD_Files/ips_to_change.txt "+group_name+"@"+server_ip+": ")
+									break
+									
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
